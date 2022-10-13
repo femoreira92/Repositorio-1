baf1ae877a2d4c1d136c93912a3e3cde463e44cf
Felipe Moreira <105679429+femoreira92@users.noreply.github.com>
2022-10-12 21:40:34 -0300

    SA 03

--- sketch.ino
+++ sketch.ino
@@ -1,11 +1,12 @@
-/** Criado por Felipe Moreira, Ricardo Sommer e Danilo Cesar;
+/**
    ESP32 + DHT22 Example for Wokwi
    
    https://wokwi.com/arduino/projects/322410731508073042
 */
 #include "WiFi.h"
 #include "DHTesp.h"
-#include "HTTPClient.h"
+#include "PubSubClient.h"
+#include "Arduino_JSON.h"
 
 #define LED 2
 
@@ -17,73 +18,112 @@ const int DHT_PIN = 15;
 const int pinoLED = 17;
 const char* ssid = "Wokwi-GUEST";
 const char* password = "";
-const String url = "https://api.thingspeak.com/update?api_key=SGMVNNLNIZ8QFSLK&";
+const String url = "https://api.thingspeak.com/update?api_key=0M7K9850R6HKN9PQ&";
 
-HTTPClient p;
+//configurações do mqtt broker
+int port = 1883; //1883
+const char* server = "broker.hivemq.com";
+const char* topico = "esp32SA3"; //"senai_tec_des"; //testtopic/2
+
+JSONVar data;
+
+WiFiClient espClient;
+PubSubClient mqttClient(espClient);
 DHTesp dhtSensor;
 
 void setup() {
   Serial.begin(115200);
-  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+  Serial.println("Iniciando Setup");
+
   WiFi.begin(ssid, password);
 
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.println(WiFi.status());
-    Serial.println("Finalizando Setup");
-  
-
+    Serial.println(".");
   }
+  Serial.println("Conectado à rede Wifi :)");
+
+//conexão com broker
+  Serial.println("Conectando ao Broker");
+  mqttClient.setServer(server, port);
+  char* clientId = "clientId-G1PBAoOhEG";//"clientID-15ioioi&7778sdfasW23"; //"esp343#edssfsd_pok" //clientId-esfwBrgOga  //clientID-29ioioi&7778sdfasW23
+
+  mqttClient.connect(clientId);
+  mqttClient.subscribe(topico);
+
+  while(!mqttClient.connected()) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Broker está conectado");  
+  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+
   pinMode(LED, OUTPUT);
   pinMode(relayPin_1, OUTPUT);
   pinMode(relayPin_2, OUTPUT);
   pinMode(relayPin_3, OUTPUT);
-  pinMode(relayPin_4, OUTPUT);
+  pinMode(relayPin_4, OUTPUT);  
 }
 
 void loop() {
+  const int relayPin_1 = 2;
+  const int relayPin_2 = 4;
+  const int relayPin_3 = 12;
+  const int relayPin_4 = 13;
+  const int DHT_PIN = 15;
+  const int pinoLED = 17;
+
   //conectando ao servidor:
-  float temperaturaa = 24.0;
-  float umidadee = 30.0;
-
-  String path = url + "field1=" + String(temperaturaa) + "&field2=" + String(umidadee);
-  p.begin(path);
-  int httpCode = p.GET();
-  String payload = p.getString();
-  Serial.println("HttpCode:" + String(httpCode));  
-  Serial.println("Payload:" + payload);
-
-  
-  //capturando temperatura e umidade
-  delay(1000);
   float temperatura = dhtSensor.getTemperature();
   float umidade = dhtSensor.getHumidity();
 
-  //imprimindo dados capturados:
-  digitalWrite(relayPin_1, HIGH);
-  delay(800);
-  Serial.println("Temperatura: " + String(temperatura, 2) + "°C");
-  Serial.println("---");
-  digitalWrite(pinoLED, !digitalRead(pinoLED));
-  digitalWrite(relayPin_1, LOW);
+  data["temperatura"] = temperatura;
+  data["umidade"] = umidade;
+  String jsonString = JSON.stringify(data);
+  if (!temperatura && !umidade){
+    Serial.println("problemas ao enviar mensagem!"); 
+  } else {
+    
+    //capturando temperatura e umidade
+    delay(1000);
+  
+    //imprimindo dados capturados:
+    digitalWrite(relayPin_1, HIGH);
+    delay(800);
+    Serial.println("Temperatura: " + String(temperatura, 2) + "°C");
+    Serial.println("---");
+    digitalWrite(pinoLED, !digitalRead(pinoLED));
+    digitalWrite(relayPin_1, LOW);
 
-  digitalWrite(relayPin_2, HIGH);
-  delay(800);
-  Serial.println("Umidade: " + String(umidade, 1) + "%");
-  Serial.println("---");
-  digitalWrite(relayPin_2, LOW);
+    digitalWrite(relayPin_2, HIGH);
+    delay(800);
+    Serial.println("Umidade: " + String(umidade, 4) + "%");
+    Serial.println("---");
+    digitalWrite(relayPin_2, LOW);
 
-  digitalWrite(relayPin_3, HIGH);
-  delay(800);
-  Serial.println("Temperatura: " + String(temperatura, 2) + "°C");
-  Serial.println("---");
-  digitalWrite(pinoLED, !digitalRead(pinoLED));
-  digitalWrite(relayPin_3, LOW);
+    digitalWrite(relayPin_3, HIGH);
+    delay(800);
+    Serial.println("Temperatura: " + String(temperatura, 12) + "°C");
+    Serial.println("---");
+    digitalWrite(pinoLED, !digitalRead(pinoLED));
+    digitalWrite(relayPin_3, LOW);
 
-  digitalWrite(relayPin_4, HIGH);
+    digitalWrite(relayPin_4, HIGH);
+    delay(800);
+    Serial.println("Umidade: " + String(umidade, 13) + "%");
+    Serial.println("---");
+    digitalWrite(relayPin_4, LOW);
+  }
+
+  boolean msg = mqttClient.publish(topico, jsonString.c_str());;
+  if (!msg) {
+    Serial.println("Problemas ao enviar msg");
+    mqttClient.connect("clientId-esfwBrgOga");
+    mqttClient.subscribe(topico);
+  } else {
+    Serial.println("Mensagem enviada com sucesso!!!");
+  }
   delay(800);
-  Serial.println("Umidade: " + String(umidade, 1) + "%");
-  Serial.println("---");
-  digitalWrite(relayPin_4, LOW);  
   
 }
